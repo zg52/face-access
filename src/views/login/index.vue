3eedebc32d2c4b0dfe84d8dc260f7a2f27e50717
Your Name <longarui@163.com>
2021-01-23 15:43:48 +0800

    修改全局路由钩子配置

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -135,7 +135,7 @@ $light_gray:#eee;
 </style>
 <template>
   <div class="login-container">
-    <div class="container_tit"><img src="../../assets/image/home-logo.png" alt=""><span>智能安防 · 监控门禁系统<svg-icon style="margin-left:4px" icon-class="sys-tit"/></span></div>
+    <div class="container_tit"><img src="../../assets/image/home-logo.png"><span>智能安防 · 监控门禁系统<svg-icon style="margin-left:4px" icon-class="sys-tit"/></span></div>
     <div class="main">
       <div class="banner">
         <img src="../../assets/image/banner.png" width="490">
@@ -329,9 +329,6 @@ export default {
 }
 </script>
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
 $bg:#283443;
 $light_gray:#fff;
 $cursor: #fff;
@@ -373,4 +370,3 @@ $cursor: #fff;
   }
 }
 </style>
-
